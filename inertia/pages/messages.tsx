dd5e60274a2eb1eb2bab24f11ca9d50937ee77c9
ahof0006 <axel.hoffmann@ulb.be>
2024-09-17 14:43:08 +0200

    messages now get user

--- inertia/pages/messages.tsx
+++ inertia/pages/messages.tsx
@@ -1,9 +1,22 @@
-import { Head } from '@inertiajs/react'
+import { Head, usePage } from '@inertiajs/react'
 
-export default function Home(props: { version: number }) {
+interface User {
+  fullName: string
+  email: string
+}
+
+interface PageProps {
+  user: User
+  [key: string]: unknown
+}
+
+export default function Messages() {
+  const { user } = usePage<PageProps>().props
+
   return (
     <>
       <Head title="Messages" />
+      <div>{user.fullName}</div>
     </>
   )
 }
